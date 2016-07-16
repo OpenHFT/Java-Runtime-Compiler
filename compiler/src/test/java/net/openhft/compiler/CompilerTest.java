100d3e0356d7f6356e92ba56ae1d5fd343167984
renato <renato@twobo.com>
2016-07-16 14:28:48 +0200

    Fixed #12 - can compile new classes after a compilation error

--- compiler/src/test/java/net/openhft/compiler/CompilerTest.java
+++ compiler/src/test/java/net/openhft/compiler/CompilerTest.java
@@ -33,6 +33,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class CompilerTest extends TestCase {
@@ -254,5 +255,36 @@ public class CompilerTest extends TestCase {
         assertFalse(usedSysErr.get());
         assertEquals("", writer.toString());
     }
+
+    public void test_compilerErrorsDoNotBreakNextCompilations() throws Exception {
+        // quieten the compiler output
+        PrintWriter quietWriter = new PrintWriter(new StringWriter());
+
+        // cause a compiler error
+        try {
+            CompilerUtils.CACHED_COMPILER.loadFromJava(
+                    getClass().getClassLoader(), "X", "clazz X {}", quietWriter);
+            fail("Should have failed to compile");
+        } catch (ClassNotFoundException e) {
+            // expected
+        }
+
+        // ensure next class can be compiled and used
+        Class testClass = CompilerUtils.CACHED_COMPILER.loadFromJava(
+                getClass().getClassLoader(), "S", "class S {" +
+                        "public static final String s = \"ok\";}");
+
+        Callable callable = (Callable)
+                CompilerUtils.CACHED_COMPILER.loadFromJava(
+                        getClass().getClassLoader(), "OtherClass",
+                        "import java.util.concurrent.Callable; " +
+                                "public class OtherClass implements Callable<String> {" +
+                                "public String call() { return S.s; }}")
+                        .newInstance();
+
+        assertEquals("S", testClass.getName());
+        assertEquals("ok", callable.call());
+    }
+
 
 }
