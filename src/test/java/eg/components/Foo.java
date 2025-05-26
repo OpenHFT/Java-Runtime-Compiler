4f5ee5120a234bece50a5a023bfa51b17006562c
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-26 11:44:03 +0100

    Add demo comment for Foo class and constructor

--- src/test/java/eg/components/Foo.java
+++ src/test/java/eg/components/Foo.java
@@ -19,12 +19,23 @@
 package eg.components;
 
 @SuppressWarnings("QuestionableName")
+/**
+ * Simple data holder used to demonstrate dynamic compilation.
+ */
 public class Foo {
     public final Bar bar;
     public final Bar copy;
     public final String s;
     public final int i;
 
+    /**
+     * Creates a new instance.
+     *
+     * @param bar  first bar dependency
+     * @param copy second bar dependency
+     * @param s    textual flag for the example
+     * @param i    example value representing some business field
+     */
     public Foo(Bar bar, Bar copy, String s, int i) {
         this.bar = bar;
         this.copy = copy;
